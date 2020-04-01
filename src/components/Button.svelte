8153a46fb930f24aa21bf77aef841c9e9027beac
fishintaiwan <fershad@gmail.com>
2020-04-01 11:38:10 +0800

    basic buttons

---
+++ src/components/Button.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let tag;
+</script>
+
+<button data-filter-tag="{tag}">{tag}</button>
+
+<style>
+button {
+    height: 40px;
+    width: 40px;
+    display: inline-block;
+}
+</style>
--- src/components/Filter.svelte
+++ src/components/Filter.svelte
@@ -1,14 +1,21 @@
 <script>
+    import Button from './Button.svelte'
 </script>
 
 <div class="filter">
     <input type="text" name="search" id="search">
+    <div class="buttons">
+        Filter by: 
+        <Button tag="H1" />
+        <Button tag="LI" />
+    </div>
 </div>
 
 <style>
     .filter {
         width: 100%;
         display: flex;
+        flex-direction: column;
         justify-content: center;
         align-items: center;
     }
@@ -16,5 +23,10 @@
     input[type="text"] {
         width: 100%;
         max-width: 280px;
+    }
+
+    .buttons {
+        display: block;
+        position: relative;
     }
 </style>
