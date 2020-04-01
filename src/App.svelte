ba8ab207bae5f0fe0a356e13d9047196a0fa2ba9
fishintaiwan <fershad@gmail.com>
2020-04-01 11:30:10 +0800

    simple search field

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,15 @@
 <script>
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+	import Filter from './components/Filter.svelte';
+	import Table from './components/Table.svelte';
 </script>
 
 <Header />
-
-<main>
-	
+<main class="container">
+	<Filter />
+	<Table />
 </main>
-
 <Footer />
 
 <style>
---
+++ src/components/Filter.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<div class="filter">
+    <input type="text" name="search" id="search">
+</div>
+
+<style>
+    .filter {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    input[type="text"] {
+        width: 100%;
+        max-width: 280px;
+    }
+</style>
