3ac4fcc41f9b8c8102022acb4014a4df3a0ba513
fishintaiwan <fershad@gmail.com>
2020-04-06 17:08:00 +0800

    move css files

--- src/App.svelte
+++ src/App.svelte
@@ -7,11 +7,23 @@
     }
 </script>
 
+<noscript>
+		<div style="height: 100vh; width: 100vw; display: flex;">
+		  <p class="h4 loud" style="margin: auto;">
+			Javascript is required to use this app. Please enable it and try again.
+		  </h1>
+		</div>
+	  </noscript>
+
 <Header />
 <main class="container">
 	<Table />
 </main>
 <Footer />
+<link rel='prefetch' href='/global.css'>
+<link rel='prefetch' href='/build/bundle.css'>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600&family=Permanent+Marker&family=Source+Code+Pro&family=Cookie&display=swap"> 
+<link rel='stylesheet' href='/typography.css'>
 
 <style>
 	main.container {
