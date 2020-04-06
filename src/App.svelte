edd69a6c7c967f5848cab1092e4d4d057a92471e
fishintaiwan <fershad@gmail.com>
2020-04-06 17:09:34 +0800

    fix noscript error

--- src/App.svelte
+++ src/App.svelte
@@ -11,7 +11,7 @@
 		<div style="height: 100vh; width: 100vw; display: flex;">
 		  <p class="h4 loud" style="margin: auto;">
 			Javascript is required to use this app. Please enable it and try again.
-		  </h1>
+		  </p>
 		</div>
 	  </noscript>
 
