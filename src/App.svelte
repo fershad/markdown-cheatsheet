02f52dda0d593f7364eba0a8b072d3226cfb8d3a
fishintaiwan <fershad@gmail.com>
2020-04-06 21:50:09 +0800

    change sw registration point

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,7 @@
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
 	import Table from './components/Table.svelte';
-	if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js');
-    }
-
+	import SW from './components/ServiceWorker.svelte';
 	window.addEventListener('beforeinstallprompt', e => {
         e.userChoice.then(choiceResult => {
           ga('send', 'event', 'Add to Homescreen', choiceResult.outcome);      
@@ -28,6 +25,7 @@
 <Footer />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600&family=Permanent+Marker&family=Source+Code+Pro&family=Cookie&display=swap"> 
 <link rel='stylesheet' href='/typography.css'>
+<SW />
 
 
 <style>
