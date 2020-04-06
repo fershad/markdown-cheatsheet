95f880e61bc08417a49662d57ec6982edad9109a
fishintaiwan <fershad@gmail.com>
2020-04-06 22:26:15 +0800

    update ga

--- src/components/ServiceWorker.svelte
+++ src/components/ServiceWorker.svelte
@@ -3,19 +3,20 @@ if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/service-worker.js');
     }
 
-window.addEventListener('beforeinstallprompt', e => {
-        e.userChoice.then(choiceResult => {
-          gtag('event', 'Install', {'event_category' : 'Add to Homescreen', 'event_label' : choiceResult.outcome});      
-        });
-      });
+window.addEventListener('appinstalled', (evt) => {
+  gtag('event', 'Install');
+});
 
 window.addEventListener('load', () => {
         if (navigator.standalone) {
           gtag('event', 'Views', {'event_category' : 'PWA', 'event_label' : 'iOS'});
+          console.log('Launched: Installed (iOS)');
         } else if (matchMedia('(display-mode: standalone)').matches) {
           gtag('event', 'Views', {'event_category' : 'PWA', 'event_label' : 'non-iOS'});
+          console.log('Launched: Installed');
         } else {
           gtag('event', 'Views', {'event_category' : 'Web'});
+          console.log('Launched: Browser Tab');
         }
       });
 </script>
